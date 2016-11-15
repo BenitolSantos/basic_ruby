e8d861447ef2e1a6b778a05c414c4dfcb3732e01
Benito Santos <benitolsantos@gmail.com>
2016-11-15 15:16:45 -0500

    age.rb

---
+++ age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+age = gets.chomp
+puts "In ten years you'll be"
+puts age.to_i + 10
+puts "In twenty years you'll be"
+puts age.to_i + 20
+puts "In thirty years you'll be"
+puts age.to_i + 30
+puts "In forty years you'll be"
+puts age.to_i + 40
