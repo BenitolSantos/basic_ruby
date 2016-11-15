b7735e81abb160f8a99a57d85abf52d8e53234ee
Benito Santos <benitolsantos@gmail.com>
2016-11-15 13:18:58 -0500

    movie_titles.rb

--- movie_titles.rb
+++ movie_titles.rb
@@ -2,3 +2,8 @@ movies = {:Age_Of_Ultron => '2015',:Batman_V_Superman => '2016',:Wonder_Woman =>
 puts movies[:Age_Of_Ultron]
 puts movies[:Batman_V_Superman]
 puts movies[:Wonder_Woman]
+
+movie_dates = [2015,2016,2017]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
