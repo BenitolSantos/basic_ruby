906305871346e8fc76679039f7bb66965a0f4198
Benito Santos <benitolsantos@gmail.com>
2016-11-15 17:06:45 -0500

    name.rb

--- name.rb
+++ name.rb
@@ -5,3 +5,9 @@ puts "Hello " + name + "!"
 10.times do
 puts name
 end
+
+puts 'Please type your first name!'
+first_name = gets.chomp
+puts 'Please type your last name!'
+last_name = gets.chomp
+puts first_name + ' ' + last_name
