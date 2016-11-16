6a60da2d55ebc28c52896bac3565987e7164c82f
Benito Santos <benitolsantos@gmail.com>
2016-11-16 18:33:05 -0500

    case_statement.rb

---
+++ say.rb
@@ -0,0 +1,32 @@
+#def say
+  # method body goes here
+#end
+
+# say.rb
+
+#puts "hello"
+#puts "hi"
+#puts "how are you"
+#puts "I'm fine"
+
+# say.rb refactored
+
+#def say(words)
+#  puts words
+#end
+
+#say("hello")
+#say("hi")
+#say("how are you")
+#say("I'm fine")
+
+# say.rb refactored
+
+#def say(words)
+#  puts words + '.'    ## <= We only make the change here!
+#end
+
+#say("hello")
+#say("hi")
+#say("how are you")
+#say("I'm fine")
