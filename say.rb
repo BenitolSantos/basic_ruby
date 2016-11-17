21cca290c6137f19fcecdbab992dc764bbaad653
Benito Santos <benitolsantos@gmail.com>
2016-11-17 08:26:15 -0500

    say.rb

--- say.rb
+++ say.rb
@@ -30,3 +30,12 @@
 #say("hi")
 #say("how are you")
 #say("I'm fine")
+
+def some_method(number)
+ number = 7 # this is implicitly returned by the method
+end
+
+a = 5
+some_method(a)
+puts a
+
---
+++ upcase.rb
@@ -0,0 +1,9 @@
+puts 'Type something!'
+type = gets.chomp
+
+if type.length > 10
+puts type.upcase
+else
+puts type
+end
+
